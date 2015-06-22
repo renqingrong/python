0cc3788d44476a2c67080eba7cfd1ed921879519
renqingrong <756380073@qq.com>
2015-06-22 16:58:49 +0800

    12

--- python.py
+++ python.py
@@ -44,33 +44,33 @@ __author__ = 'EasyShare25'
    #
    #  print"Current Letter:",letter
 
-def sub_sort(array,low,high):
-    key = array[low]
-    while low < high:
-        while low < high and array[high] >= key:
-            high -= 1
-        while low < high and array[high] < key:
-            array[low] = array[high]
-            low += 1
-            array[high] = array[low]
-    array[low] = key
-    return low
-
-
-def quick_sort(array,low,high):
-     if low < high:
-        key_index = sub_sort(array,low,high)
-        quick_sort(array,low,key_index)
-        quick_sort(array,key_index+1,high)
-if __name__ == '__main__':
-    array = [{"name":"Tom", "age":18 },
-        {"name":"Alice", "age":28 },
-        {"name":"Bruce", "age":8 },
-        {"name":"Smith", "age":13 },
-        {"name":"Cheery", "age":8 }]
-    quick_sort(array,0,len(array)-1)
-    print array
-
+# def sub_sort(array,low,high):
+#     key = array[low]
+#     while low < high:
+#         while low < high and array[high] >= key:
+#             high -= 1
+#         while low < high and array[high] < key:
+#             array[low] = array[high]
+#             low += 1
+#             array[high] = array[low]
+#     array[low] = key
+#     return low
+#
+#
+# def quick_sort(array,low,high):
+#      if low < high:
+#         key_index = sub_sort(array,low,high)
+#         quick_sort(array,low,key_index)
+#         quick_sort(array,key_index+1,high)
+# if __name__ == '__main__':
+#     array = [{"name":"Tom", "age":18 },
+#         {"name":"Alice", "age":28 },
+#         {"name":"Bruce", "age":8 },
+#         {"name":"Smith", "age":13 },
+#         {"name":"Cheery", "age":8 }]
+#     quick_sort(array,0,len(array)-1)
+#     print array
+#aa
 sum=0
 i=1
 for i in range(1001):
